6bb12e348f1e8730635518a3d13e14cad7a9624a
Jihunkim95 <darak551@naver.com>
2024-01-29 18:08:38 +0900

    init : 초기 설정

---
+++ AppleLoginDemo/AppleLoginDemo/AppleLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginDemoApp.swift
+//  AppleLoginDemo
+//
+//  Created by 김지훈 on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleLoginDemo/AppleLoginDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleLoginDemo
+//
+//  Created by 김지훈 on 2024/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
